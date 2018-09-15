a19509d5621a85afdaad98dd23402ad0449fb896
Cristiano Pimenta <cgpimenta@dcc.ufmg.br>
2018-09-15 12:57:12 -0300

    Data preprocessing

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,28 +1,20 @@
+#!/usr/bin/env python
 
+import pandas as pd
 import sys
 
-
-def read_csv(filename, sep=',', skiprows=0, skipcols=0):
-    matrix = []
-    with open(filename, 'r') as f:
-        for i in range(skiprows):
-            f.readline()
-        matrix = [list(line.split(sep)[skipcols:]) for line in f]
-
-    return matrix
-
-
-def save_csv(matrix, output_filename, sep=','):
-    with open(output_filename, 'w') as f:
-        for row in matrix:
-            f.write(sep.join(row))
-
+from random import uniform
 
 input_filename = sys.argv[1]
 output_filename = sys.argv[2]
-skiprows = int(sys.argv[3])
-skipcols = int(sys.argv[4])
 
+df = pd.read_csv(input_filename)
 
-save_csv(read_csv(input_filename, skiprows=skiprows, skipcols=skipcols),
-         output_filename)
+df = df.select_dtypes(include=['number'])
+
+max_val = df.max().max()
+min_val = df.min().min()
+
+df = df.fillna(uniform(min_val, max_val))
+
+df.to_csv(output_filename, index=False, header=None)
